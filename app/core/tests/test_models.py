3029c3c9f38d5cf7fa2da8c5a5f3fc046e7d95de
Josh Bright <josh.bright@gmail.com>
2022-09-01 18:21:27 -0700

    Added tests for checking emails on users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,17 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_funny_email_gets_normalized(self):
+        sample_emails = [
+            ["test1@EXAMPLE.COM", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@example.COM", "TEST3@example.com"],
+            ["Test4@example.COM", "Test4@example.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email, password="sample123"
+            )
+            self.assertEqual(user.email, expected)
