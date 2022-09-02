dcc51a83902cba5f7a9b1b1d68fd9f0704a92ea7
Josh Bright <josh.bright@gmail.com>
2022-09-01 18:40:58 -0700

    Added test to see if we can create a user without an email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,8 @@ class ModelTests(TestCase):
                 email=email, password="sample123"
             )
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a new user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "sample123")
