fcedb894f0867111b3e7f78eeb9c73fcc621b9f5
Josh Bright <josh.bright@gmail.com>
2022-09-01 21:12:24 -0700

    Added more to the user admin class to get the edit user page to work

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("name", "email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important Dates", {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+
+
+admin.site.register(models.User, UserAdmin)
